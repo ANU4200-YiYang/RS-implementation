c9db3e279ff0212f5045fd0be5e3ea982a9ada5b
ANU4200-YiYang <77652152+ANU4200-YiYang@users.noreply.github.com>
2021-04-14 21:29:40 +0800

    Add files via upload

--- NCF_GU/main.py
+++ NCF_GU/main.py
@@ -9,6 +9,7 @@ import model
 import config
 import evaluate
 import data_utils
+from torch.utils.tensorboard import SummaryWriter
 
 
 # environment
@@ -17,6 +18,8 @@ cudnn.benchmark = True
 # train,test dataset
 train_data, train_mat, test_data, user_num, item_num = data_utils.load_all()
 train_dataset = data_utils.NCFData(train_data, user_num, item_num, train_mat, 4, True)
+# print(train_dataset)
+# exit()
 train_loader = data.DataLoader(train_dataset, batch_size=256, shuffle=True, num_workers=0)
 test_dataset = data_utils.NCFData(test_data, user_num, item_num, train_mat, 0, False)
 # cant change batch size of test dataset because of test strategy
@@ -39,11 +42,11 @@ if config.model == 'NeuMF-pre':
 	optimizer = optim.SGD(model.parameters(), lr=0.001)
 else:
 	optimizer = optim.Adam(model.parameters(), lr=0.001)
-
+writer = SummaryWriter()
 best_epoch = 0
 best_hr = 0
 best_ndcg = 0
-for epoch in range(1):
+for epoch in range(20):
 	model.train()
 	#
 	train_loader.dataset.ng_sample()
@@ -55,18 +58,21 @@ for epoch in range(1):
 		label = label.float().cuda()
 		# print(user)
 		# print(item)
-		# print(label)
-		# exit()
+		print(label)
+		exit()
 
 		model.zero_grad()
 		prediction = model(user, item)
 		loss = loss_function(prediction, label)
 		loss.backward()
 		optimizer.step()
+		writer.add_scalar("Loss/train", loss, epoch)
 		count += 1
 		print("step:", count)
+	writer.flush()
 	# evaluation
 	model.eval()
+	writer.close()
 	HR, NDCG = evaluate.metrics(model, test_loader, 10)
 	print(r"HR: {:.3f} NDCG: {:.3f}".format(np.mean(HR), np.mean(NDCG)))
 	# save best HR
@@ -77,3 +83,5 @@ for epoch in range(1):
 		torch.save(model.state_dict(), '{}{}.pth'.format(config.model_path, config.model))
 	print("epoch:", epoch+1)
 print("End. Best epoch {:03d}: HR = {:.3f}, NDCG = {:.3f}".format(best_epoch, best_hr, best_ndcg))
+# End. Best epoch 009: HR = 0.694, NDCG = 0.423
+# cold：End. Best epoch 006: HR = 0.677, NDCG = 0.408
